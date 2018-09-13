df436acb59997856351c3183962fe581f1bba615
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-09-13 08:35:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  for (var i=0 ; i < dwarves.length; i++) {
+  return (i + 1) + '. ' + dwarves[i] + ' ' + (i + 2) + '. ' + dwarves[i + 1] + ' ' + (i + 3) + '. '+ dwarves[i + 2] + ' ';
+}
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var array = []
+  for (var i= 0; i < planeteerCalls.length; i++) {
+  array.push(planeteerCalls[i].toUpperCase() + '!' );
+  }
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i= 0; i < words.length; i++) {
+    if ( words[i].length > 4) {
+    return true;
+    }
+  else {
+  return false; 
+  }
+  }  
 }
 
 function findTheCheese (foods) {
-}
+  for (var i= 0; i < foods.length; i++) {
+   if ( foods[i] === 'cheddar') {
+     return foods[i];
+   }
+  }
+   if (foods !== 'cheddar') {
+     return 'no cheese!';
+   }
+  }
+
